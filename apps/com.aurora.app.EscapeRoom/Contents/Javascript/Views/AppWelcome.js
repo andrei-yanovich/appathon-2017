a8e92a8844d3b5a88e8879990a85c326a4fef699
Andrei Yanovich <yanovich.andrei@gmail.com>
2017-09-19 15:59:53 +0200

    AppWelcome

--- apps/com.aurora.app.EscapeRoom/Contents/Javascript/Views/AppWelcome.js
+++ apps/com.aurora.app.EscapeRoom/Contents/Javascript/Views/AppWelcome.js
@@ -6,38 +6,85 @@ var AppWelcome = new MAF.Class( {
     //  1920 X 1080
     // Create your view template
     createView: function() {
-        var deviceSelection = this.deviceSelection = new MAF.control.Button({
-            focus: false,
+        var deviceSelection = this.deviceSelection = new MAF.control.TextButton({
+            theme: false,
+            label: 'Device Selection',
+            textStyles: {
+                anchorStyle: 'center',
+                fontSize: 40,
+                marginTop: -4
+            },
             styles: {
                 width: 431,
                 height: 98,
                 hOffset: 1380,
                 vOffset: 769,
-                backgroundImage: 'Images/Escape_button_Empty.png'
+                borderRadius: 15,
+                paddingTop: 2,
+                border: '3px solid #847d75',
+                backgroundColor: '#4a4a4a'
             },
             events: {
+                onBlur: function () {
+                    this.setStyles({
+                        paddingTop: 2,
+                        border: '3px solid #847d75'
+                    });
+                    this.children[0].setStyles({
+                        fontSize: 40
+                    });
+                },
                 onFocus: function () {
                     this.setStyles({
-                        backgroundImage: 'Images/Escape_button_Empty_focussed.png'
+                        paddingTop: 0,
+                        border: '5px solid #edddce'
+                    });
+                    this.children[0].setStyles({
+                        fontSize: 44
                     });
                 }
             }
         }).appendTo(this);
 
-        var startPlaying = this.startPlaying = new MAF.control.Button({
-            focus: false,
+        var startPlaying = this.startPlaying = new MAF.control.TextButton({
+            label: 'Start Playing',
+            theme: false,
+            textStyles: {
+                anchorStyle: 'center',
+                fontSize: 40,
+                marginTop: -4
+            },
             styles: {
                 width: 431,
                 height: 98,
                 hOffset: 1380,
                 vOffset: 902,
-                backgroundImage: 'Images/Escape_button_Empty.png'
+                borderRadius: 15,
+                paddingTop: 2,
+                border: '3px solid #847d75',
+                backgroundColor: '#4a4a4a'
             },
             events: {
+                onBlur: function () {
+                    this.setStyles({
+                        paddingTop: 2,
+                        border: '3px solid #847d75'
+                    });
+                    this.children[0].setStyles({
+                        fontSize: 40
+                    });
+                },
                 onFocus: function () {
                     this.setStyles({
-                        backgroundImage: 'Images/Escape_button_Empty_focussed.png'
+                        paddingTop: 0,
+                        border: '5px solid #edddce'
                     });
+                    this.children[0].setStyles({
+                        fontSize: 44
+                    });
+                },
+                onSelect: function () {
+                    MAF.application.loadView('RoomSelector');
                 }
             }
         }).appendTo(this);
